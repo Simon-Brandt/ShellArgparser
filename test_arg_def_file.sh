923c6671c41174bfe3922845ce234cdf6c37a15b
Simon-Brandt <simon.brandt@uni-greifswald.de>
2024-12-03 16:40:00 +0100

    Added test for separate arguments definition file

---
+++ test_arg_def_file.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Author: Simon Brandt
+# E-Mail: simon.brandt@uni-greifswald.de
+# Last Modification: 2024-12-03
+
+# Usage: Run this script with "bash test_arg_def_file.sh".
+
+# Purpose: Test the functionality of the argparser.
+
+# Set the argparser to read the arguments definition from a file.
+export ARGPARSER_ARG_DEF_FILE="arguments.lst"
+export ARGPARSER_MAX_COL_WIDTH_1=9
+export ARGPARSER_MAX_COL_WIDTH_2=33
+export ARGPARSER_MAX_COL_WIDTH_3=35
+
+# Set the arguments.
+args=(
+    var_1
+    var_2
+    var_3
+    var_4
+    var_5
+    var_6
+)
+
+set -o nounset
+(set -o posix; set) > 1.txt
+source argparser.sh --read -- "$@"
+source argparser.sh --set -- "$@"
+(set -o posix; set) > 2.txt
+# diff --side-by-side --suppress-common-lines 1.txt 2.txt
+
+# The arguments can now be accessed as keys and values of the
+# associative array "args".  Further, they are set as variables to the
+# environment.  If positional arguments were given, they are set to $@.
+for arg in "${!args[@]}"; do
+    printf "The keyword argument \"%s\" equals \"%s\".\n" \
+        "${arg}" "${args[${arg}]}"
+done | sort
+
+(( i = 1 ))
+for arg in "$@"; do
+    printf "The positional argument on index \"%s\" equals \"%s\".\n" \
+        "${i}" "${arg}"
+    (( i++ ))
+done
