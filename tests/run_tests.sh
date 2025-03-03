107641c84e3d43c2328ad78cd4185b6865e8cdff
Simon-Brandt <simon.brandt@uni-greifswald.de>
2025-03-03 14:49:04 +0100

    Added `run_tests.sh` to execute the test scripts and perform diffs

---
+++ tests/run_tests.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+
+# Author: Simon Brandt
+# E-Mail: simon.brandt@uni-greifswald.de
+# Last Modification: 2025-03-03
+
+# TODO: Add tests for the general arguments parsing, for configuration
+#       files, arguments definition files, help files, and localization.
+# TODO: Write test files for keyword-only and positional-only arguments.
+
+# Usage: Run this script with "bash run_tests.sh".
+
+# Purpose: Test the functionality of the argparser by running all test
+# scripts and comparing their output to expected values.
+
+# Define the functions for printing the colored output.
+function print_separator() {
+    # Print a colored line of 120 characters acting as visual separator.
+    #
+    # Arguments:
+    # - $1: the character to use as separator
+    # - $2: the ANSI SGR escape sequence for colorization
+
+    printf '\e[%s;1m' "$2"
+    printf '%120s' "" | tr ' ' "$1"
+    printf '\e[0m\n'
+}
+
+function print_single_separator() {
+    # Print a line of 120 hyphens acting as visual separator, colored in
+    # white.
+    print_separator "-" 37
+}
+
+function print_double_separator() {
+    # Print a line of 120 equals signs acting as visual separator,
+    # colored in blue.
+    print_separator "=" 36
+}
+
+# Define the function for printing the section names.
+function print_section() {
+    # Print the section name.
+    #
+    # Arguments:
+    # - $1: the section name to print
+    printf '\e[33;1;7mRunning tests for %s...' "$1"
+    printf '%*s' $(( 99 - ${#1} )) ""
+    printf '\e[0m\n'
+    print_double_separator
+}
+
+# Define the function for printing the diff.
+function print_diff() {
+    # Print the difference between the command's output and the expected
+    # output.
+    #
+    # Arguments:
+    # - $1: the command line to run
+    # - $2: the expected output
+    printf 'Running test %s: "\e[1m%s\e[0m"...\n' "${test_number}" "$1"
+    diff --side-by-side --suppress-common-lines --color=always --width=120 \
+        <(eval "$1") \
+        <(printf '%s\n' "$2") \
+        >&2
+    # shellcheck disable=SC2181  # Intentional explicit check for return value.
+    if (( "$?" == 0 )); then
+        printf '\e[32;1;7mTest %s succeeded with correct output.' \
+            "${test_number}"
+        printf '%*s' $(( 84 - "${#test_number}" )) ""
+        printf '\e[0m\n'
+    elif (( "$?" == 1 )); then
+        print_single_separator
+        printf '\e[31;1;7mTest %s failed with diverging output.' \
+            "${test_number}"
+        printf '%*s' $(( 85 - "${#test_number}" )) ""
+        printf '\e[0m\n'
+    fi
+    print_double_separator
+}
+
+# Run the tests.
+# 1.    Test the general functionality using test_basic.sh.
+# 2.    Test the functionality regarding short options.
+print_section "short options"
+
+# 2.1.  Test the normal output.
+test_number="2.1"
+cmd="bash test_short_options.sh 1 2 -a 1 -b 2 -c A"
+output="$(cat << EOF
+The keyword argument "var_1" is set to "1".
+The keyword argument "var_2" is set to "2".
+The keyword argument "var_3" is set to "A".
+The keyword argument "var_4" is set to "A".
+The keyword argument "var_5" is set to "E".
+The keyword argument "var_6" is set to "false".
+The keyword argument "var_7" is set to "true".
+The positional argument "pos_1" on index 1 is set to "2".
+The positional argument "pos_2" on index 2 is set to "1,2".
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 2.2.  Test the usage message in "row" orientation.
+test_number="2.2"
+cmd="bash test_short_options.sh -u"
+output="$(cat << EOF
+Usage: test_short_options.sh [-h | -u | -V] [-d,-D={A,B,C}] [-e,-E=VAL_5] [-f,-F] [-g,-G] -a,-A=VAL_1 -b,-B=VAL_2... -c,-C={A,B}... [{1,2}] pos_2
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 2.3.  Test the usage message in "column" orientation.
+test_number="2.3"
+cmd="ARGPARSER_USAGE_MESSAGE_ORIENTATION=column bash test_short_options.sh -u"
+output="$(cat << EOF
+Usage: test_short_options.sh [-h | -u | -V]
+                             [-d,-D={A,B,C}]
+                             [-e,-E=VAL_5]
+                             [-f,-F]
+                             [-g,-G]
+                             -a,-A=VAL_1
+                             -b,-B=VAL_2...
+                             -c,-C={A,B}...
+                             [{1,2}]
+                             pos_2
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 2.4.  Test the help message.
+test_number="2.4"
+cmd="bash test_short_options.sh -h"
+output="$(cat << EOF
+Usage: test_short_options.sh [OPTIONS] ARGUMENTS [--] [pos_1] pos_2
+
+Mandatory arguments to long options are mandatory for short options too.
+
+Positional arguments:
+[pos_1={1,2}]      one positional argument with default and choice (default:
+                    2)
+pos_2              two positional arguments without default or choice
+
+Mandatory options:
+-a=VAL_1,-A=A      one value without default or choice
+-b=VAL_2,-B=B      at least one value without default or choice
+-c={A,B},-C={A,B}  at least one value with choice
+
+Optional options:
+[-d={A,B,C}],      one value with default and choice (default: A)
+[-D={A,B,C}]
+[-e=VAL_5], [-E=E] one value with default (default: E)
+[-f, -F]           no value (flag) with default (default: false)
+[-g, -G]           (DEPRECATED) no value (flag) with default (default: true)
+
+-h                 display this help and exit
+-u                 display the usage and exit
+-V                 display the version and exit
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 3.    Test the functionality regarding long options.
+print_section "long options"
+
+# 3.1.  Test the normal output.
+test_number="3.1"
+cmd="bash test_long_options.sh 1 2 --var-a 1 --var-b 2 --var-c A"
+output="$(cat << EOF
+The keyword argument "var_1" is set to "1".
+The keyword argument "var_2" is set to "2".
+The keyword argument "var_3" is set to "A".
+The keyword argument "var_4" is set to "A".
+The keyword argument "var_5" is set to "E".
+The keyword argument "var_6" is set to "false".
+The keyword argument "var_7" is set to "true".
+The positional argument "pos_1" on index 1 is set to "2".
+The positional argument "pos_2" on index 2 is set to "1,2".
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 3.2.  Test the usage message in "row" orientation.
+test_number="3.2"
+cmd="bash test_long_options.sh --usage"
+output="$(cat << EOF
+Usage: test_long_options.sh [-h | -u | -V] [--var-4,--var-d={A,B,C}] [--var-5,--var-e=VAL_5] [--var-6,--var-f] [--var-7,--var-g] --var-1,--var-a=VAL_1 --var-2,--var-b=VAL_2... --var-3,--var-c={A,B}... [{1,2}] pos_2
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 3.3.  Test the usage message in "column" orientation.
+test_number="3.3"
+cmd="ARGPARSER_USAGE_MESSAGE_ORIENTATION=column bash test_long_options.sh --usage"
+output="$(cat << EOF
+Usage: test_long_options.sh [-h | -u | -V]
+                            [--var-4,--var-d={A,B,C}]
+                            [--var-5,--var-e=VAL_5]
+                            [--var-6,--var-f]
+                            [--var-7,--var-g]
+                            --var-1,--var-a=VAL_1
+                            --var-2,--var-b=VAL_2...
+                            --var-3,--var-c={A,B}...
+                            [{1,2}]
+                            pos_2
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 3.4.  Test the help message.
+test_number="3.4"
+cmd="bash test_long_options.sh --help"
+output="$(cat << EOF
+Usage: test_long_options.sh [OPTIONS] ARGUMENTS [--] [pos_1] pos_2
+
+Mandatory arguments to long options are mandatory for short options too.
+
+Positional arguments:
+[pos_1={1,2}]                    one positional argument with default and
+                                 choice (default: 2)
+pos_2                            two positional arguments without default or
+                                 choice
+
+Mandatory options:
+--var-1=VAL_1, --var-a=VAR-A     one value without default or choice
+--var-2=VAL_2, --var-b=VAR-B     at least one value without default
+                                 or choice
+--var-3={A,B}, --var-c={A,B}     at least one value with choice
+
+Optional options:
+[--var-4={A,B,C}],               one value with default and choice
+[--var-d={A,B,C}]                (default: A)
+[--var-5=VAL_5], [--var-e=VAR-E] one value with default (default: E)
+[--var-6, --var-f]               no value (flag) with default (default: false)
+[--var-7, --var-g]               (DEPRECATED) no value (flag) with default
+                                 (default: true)
+
+--help                           display this help and exit
+--usage                          display the usage and exit
+--version                        display the version and exit
+EOF
+)"
+print_diff "${cmd}" "${output}"
+
+# 4.    Test the functionality regarding configuration files.
+# 5.    Test the functionality regarding arguments definition files.
+# 6.    Test the functionality regarding help files.
+# 7.    Test the functionality regarding the localization.
